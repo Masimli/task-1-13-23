b9f57d7c35bffaba08c0af463f2642b1240a72a4
Ilkin Masimli <ilkin.masimli98@gmail.com>
2023-01-13 08:58:36 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//int number = 100;
+//int sum = 0;
+
+//for (int i = number; i >= 0; i--)
+//{ 
+//	if (i % 13 == 0)
+//	{
+//        sum += i;
+//        Console.WriteLine(i);
+//    }
+//}
+//Console.WriteLine(sum);
+
+//int n = 0;
+//int sum = 0;
+//while (n != 0)
+//{
+//    sum += n % 10;
+//    n /= 10;
+//}
+//if (sum % 2 == 0)
+//{
+//    Console.WriteLine("sum is even");
+//}
+//else { Console.WriteLine("sum is odd"); }
+
+//int number = 6721;
+//int min = 9;
+//int k;
+
+//while (number > 0)
+//{
+//    k = number % 10;
+//    if (k < min)
+//    {
+//        min = k;
+//    }
+//    number = number / 10;
+//}
+//Console.WriteLine(min);
